521facfa1bd2335ee26db056a9646aec3bf6b491
cyrus <cryrus1999coding@gmail.com>
2026-08-10 09:45:15 +0330

    010 Code documentation through Summary

--- CleanCode/Program.cs
+++ CleanCode/Program.cs
@@ -10,20 +10,47 @@ namespace CleanCode
             Console.ReadKey();
         }
 
-        public class MathUtils
+        public class Calculator
         {
             /// <summary>
-            /// TODO: Implement the binary search algoritm
+            /// Adds two numbers together and returns the result.
             /// </summary>
-            /// <param name="sortedArray"></param>
-            /// <param name="target"></param>
-            /// <returns></returns>
-            public static int BinarySearch(int[] sortedArray, int target)
+            /// <param name="a">
+            /// The first number to add.
+            /// </param>
+            /// <param name="b">
+            /// The second number to add.
+            /// </param>
+            /// <returns>
+            /// The sum of <paramref name="a"/> and <paramref name="b"/>.
+            /// </returns>
+            public static int Add(int a, int b)
             {
+                return a + b;
+            }
 
-                return -1;
+            /// <summary>
+            /// Divides one number by another number.
+            /// </summary>
+            /// <param name="a">
+            /// The number that will be divided.
+            /// </param>
+            /// <param name="b">
+            /// The number to divide by.
+            /// </param>
+            /// <returns>
+            /// The result of dividing <paramref name="a"/> by <paramref name="b"/>.
+            /// </returns>
+            /// <exception cref="DivideByZeroException">
+            /// Thrown when <paramref name="b"/> is zero.
+            /// </exception>
+            public static double Divide(double a, double b)
+            {
+                if (b == 0)
+                    throw new DivideByZeroException();
+
+                return a / b;
             }
         }
-
     }
 }
---
+++ CleanCode/docs/010-Code-documentation-through-Summary.md
@@ -0,0 +1,51 @@
+﻿# 010 Code documentation through Summary
+
+```cs
+using System;
+
+/// <summary>
+/// Provides mathematical operations for working with numbers.
+/// </summary>
+public class Calculator
+{
+    /// <summary>
+    /// Adds two numbers together and returns the result.
+    /// </summary>
+    /// <param name="a">
+    /// The first number to add.
+    /// </param>
+    /// <param name="b">
+    /// The second number to add.
+    /// </param>
+    /// <returns>
+    /// The sum of <paramref name="a"/> and <paramref name="b"/>.
+    /// </returns>
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+
+    /// <summary>
+    /// Divides one number by another number.
+    /// </summary>
+    /// <param name="a">
+    /// The number that will be divided.
+    /// </param>
+    /// <param name="b">
+    /// The number to divide by.
+    /// </param>
+    /// <returns>
+    /// The result of dividing <paramref name="a"/> by <paramref name="b"/>.
+    /// </returns>
+    /// <exception cref="DivideByZeroException">
+    /// Thrown when <paramref name="b"/> is zero.
+    /// </exception>
+    public double Divide(double a, double b)
+    {
+        if (b == 0)
+            throw new DivideByZeroException();
+
+        return a / b;
+    }
+}
+```
